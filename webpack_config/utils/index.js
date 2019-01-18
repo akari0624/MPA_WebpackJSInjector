0eb375d661302a58ede992c97268ce6216554fc1
akari0624 <akari0624@hotmail.com>
2019-01-19 00:28:25 +0800

    the fileName of HTMLPlugin is base on webpack output-path

--- webpack_config/utils/index.js
+++ webpack_config/utils/index.js
@@ -7,8 +7,8 @@ exports.generateMultipleWebpackHTMLPluginInstanceInOneArr =
        return jspFilePathArr.map(p => {
 
        let conf = {
-        template: path.join('../', `./${p}`),  // 要inject的檔案在哪裡
-        filename: path.join( `./builds/${p}`)  // inject完之後  要把重新產生的檔案放在哪裡
+        template: path.join('../', `./${p}`),  // 要inject的檔案在哪裡  這裡是讓他跑去JS的上一層，看JSP放哪裡 自行調整
+        filename: path.join( `./${p}`)  // inject完之後  要把重新產生的檔案放在哪裡, 這路徑會以webpack的output-path為基準
        }
 
        return new HtmlWebPackPlugin(conf)
