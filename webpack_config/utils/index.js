6b185ef1a3b34429ad71f2cd18108b4a4b7386e7
akari0624 <akari0624@hotmail.com>
2019-01-20 00:33:07 +0800

    被webpavkHTMLPlugin inject過後的檔案不能放在webpack的root path底下，因為他們會被當作靜態檔案處理

--- webpack_config/utils/index.js
+++ webpack_config/utils/index.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+const templateBuildedEmitPath = require('./templateBuildsConfig').templateBuildedEmitPath
 
 exports.generateMultipleWebpackHTMLPluginInstanceInOneArr = 
     jspFilePathArr => {
        return jspFilePathArr.map(p => {
 
        let conf = {
-        template: path.join('../', `./${p}`),  // 要inject的檔案在哪裡  這裡是讓他跑去JS的上一層，看JSP放哪裡 自行調整
-        filename: path.join( `./${p}`)  // inject完之後  要把重新產生的檔案放在哪裡, 這路徑會以webpack的output-path為基準
+        template: path.join('../', `./${p}`),  // 要inject的檔案在哪裡
+
+        // inject完之後  要把重新產生的檔案放在哪裡, 這路徑會以webpack的output-path為基準
+        // 這個路徑不能在devServer的root裡，不然devServer會把它當靜態檔案提供  瀏覽器上就變成在下載檔案
+        filename: path.join( '../', `./${templateBuildedEmitPath}/${p}`)  
        }
 
        return new HtmlWebPackPlugin(conf)
