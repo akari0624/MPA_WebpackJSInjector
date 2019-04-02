e00ec7f1d4116193f7a199ab7de6a7f91b53e812
akari0624 <akari0624@hotmail.com>
2019-04-03 00:54:03 +0800

    move production bundle to dist directory

--- webpack_config/utils/index.js
+++ webpack_config/utils/index.js
@@ -12,7 +12,7 @@ exports.generateMultipleWebpackHTMLPluginInstanceInOneArr =
 
         // inject完之後  要把重新產生的檔案放在哪裡, 這路徑會以webpack的output-path為基準
         // 這個路徑不能在devServer的root裡，不然devServer會把它當靜態檔案提供  瀏覽器上就變成在下載檔案
-        filename: path.join( '../', `./${templateBuildedEmitPath}/${p}`),
+        filename: process.env.IS_PRODUCT === 'Y' ? path.join( '../dist','WEB-INF', 'views', `./${templateBuildedEmitPath}/${p}`) : path.join( '../','WEB-INF', 'views', `./${templateBuildedEmitPath}/${p}`),
         chunks:[p],  
        }
 
